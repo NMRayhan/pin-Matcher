535d91f54202394bdc1c9bc4bbe9c5c79fdccd4a
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 00:52:31 +0600

    Comment added and Backspace Function Code Added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Random Generate function
 function RandomPin(){
     const pin = Math.random()*10000
     const result = (pin + '').split('.')[0]
@@ -8,33 +9,35 @@ function RandomPin(){
     }
 }
 
+// Random Generate
 document.getElementById('pin-Generate').addEventListener('click', function () {
     let randomNumber = RandomPin()
     document.getElementById('pin-Generate-Input').value = randomNumber;
 })
 
-
-// document.getElementById('pin-Generate').addEventListener('click', function () {
-//     let randomNumber = Math.ceil(Math.random() * 10000);
-//     document.getElementById('pin-Generate-Input').value = randomNumber;
-// })
-
+//Backspace function Code
+function back() {
+    var value = document.getElementById("pin-Input").value;
+    document.getElementById("pin-Input").value = value.substr(0, value.length - 1);
+}
 
 //notification are by Default Hide Access by Array
 var notify = document.getElementsByClassName('notify');
 notify[0].style.display = 'none'
 notify[1].style.display = 'none'
 
+
+//Digit Added in Input field using event Handle
 document.getElementById('digit-container').addEventListener('click', function (event) {
     document.getElementById('backSpace').innerText = 'B'
     const digit = event.target.innerText
     if (isNaN(digit)) {
-        if(digit == 'C'){
+        if(digit === 'C'){
             document.getElementById('backSpace').innerText = ''
             document.getElementById('pin-Input').value = ''
         }if(digit == 'B'){
             document.getElementById('backSpace').innerText = ''
-            console.log("B");
+            back();
         }
     } else {
         document.getElementById('backSpace').innerText = ''
@@ -43,6 +46,8 @@ document.getElementById('digit-container').addEventListener('click', function (e
     }
     document.getElementById('backSpace').innerText = ''
 })
+
+// pin match functon 
 function pinMatch(){
     let generatePin = document.getElementById('pin-Generate-Input').value;
     let generatePinNumber = parseInt(generatePin)
